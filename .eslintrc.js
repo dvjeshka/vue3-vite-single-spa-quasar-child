c2d38f2849d529ca1448ede1fd7af604934dbc36
dvjeshko <r.bochkarev@bk.ru>
2022-06-23 13:15:27 +0300

    update

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 2022,
+  },
+  plugins: [],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  },
+}
